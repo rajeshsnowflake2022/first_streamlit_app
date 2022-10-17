6949f7d03325fbbdbadf75e8ac5c86d159891a81
Rajesh Govindaraj <115979293+rajeshsnowflake2022@users.noreply.github.com>
2022-10-17 13:51:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,10 @@ streamlit.header("Lerning");
 
 streamlit.text("Snowflake");
 
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
+
+
